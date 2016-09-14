c7dc5d9130de5e064b6e9f33c1cd514dd088a1e3
Cory Mathis <coryrmathis@gmail.com>
2016-09-14 13:43:51 -0500

    Debug hamster program, support invalid input.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,24 +1,64 @@
 puts "What's the hamster's name?"
 name = gets.chomp
-puts "How loud is the hamster? Rate 1-10"
-volume = gets.chomp
-volume = volume.to_i
-puts "What is it's fur color?"
-color = gets.chomp
-puts "Is the hamster a good candidate for adoption? Y/N?"
-good = gets.chomp
-if good == "Y"
-	good = true
-else
-	good = false
-end
-good = good.to_s
-puts "What is it's age?"
-age = gets.chomp
-age = age.to_i
-puts "Name: #{name}"
-puts "Volume: #{volume}"
-puts "Color: #{color}"
-puts "Good Candidate: #{good}"
-puts "Estimated Age: #{age}"
+name = name.split.map(&:capitalize).join(' ')
 
+puts ""
+valid_input = false
+until valid_input
+	puts "How loud is the hamster (1-10)?."
+	volume = gets.chomp
+	volume = volume.to_f
+	if volume > 0 && volume <= 10
+		valid_input = true
+	else
+		puts ""
+		puts "I didn't understand you."
+	end
+end
+
+puts ""
+puts "What is its fur color?"
+color = gets.chomp
+color = color.capitalize
+
+puts ""
+valid_input = false
+until valid_input
+	puts "Is the hamster a good candidate for adoption (y/n)?"
+	good = gets.chomp
+	good = good.downcase
+	if good == "y"
+		good = true
+		valid_input = true
+	elsif good == "n"
+		good = false
+		valid_input = true
+	else
+		puts ""
+		puts "I didn't understand you."
+	end
+end
+
+puts ""
+valid_input = false
+until valid_input
+	puts "What is its age?"
+	age = gets.chomp
+	if age == ""
+		age = nil
+		valid_input = true
+	elsif age.to_f > 0 
+		age = age.to_f
+		valid_input = true
+	else
+		puts ""
+		puts "I didn't understand you."
+	end
+end
+
+puts ""
+puts "             Name:     #{name}"
+puts "           Volume:     #{volume}"
+puts "            Color:     #{color}"
+puts "   Good Candidate:     #{good.to_s.capitalize}"
+puts "    Estimated Age:     #{age}"
