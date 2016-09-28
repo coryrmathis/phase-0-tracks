4f6e3712b6042a44cd0ccd0f408a29da6e2588ac
Cory Mathis <coryrmathis@gmail.com>
2016-09-28 11:24:23 -0500

    Add Puppy class methods including initialize, dog_years, and learn_trick

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,11 +1,41 @@
 class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+	def initialize
+		puts "Initializing new puppy instance..."
+	end
 
+	def fetch(toy)
+    	puts "I brought back the #{toy}!"
+    	toy
+    end
+
+	def speak(integer)
+  		integer.times do 
+  		puts "Woof!"
+  		end
+  	end
+
+  	def roll_over
+  		puts "*rolls over*"
+  	end
+
+	def dog_years(human_years)
+		dog_years = human_years * 7
+		puts "This dog is #{dog_years} in dog years."
+	end
+
+	
+	def learn_trick(trick)
+		trick_array = []
+		trick_array << trick
+		p trick_array
+	end
 end
 
 spot = Puppy.new
 spot.fetch("rope")
+spot.speak(5)
+spot.roll_over
+spot.dog_years(4)
+spot.learn_trick("Play Dead")
+spot.learn_trick("Speak")
