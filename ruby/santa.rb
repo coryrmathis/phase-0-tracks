44536ee57a2c9eee005eb11f2226c8fbe4ce9c45
Cory Mathis <coryrmathis@gmail.com>
2016-09-28 14:39:08 -0500

    Define .eat_milk_and_cookies instance method. Initialize new instances with gender, ethnicity, and array of reindeer to be rated.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,17 @@
 class Santa
-	
+
+	def initialize
+		puts "Initializing Santa instance..."
+	end
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
 	end
+
+	def eat_milk_and_cookies(cookie)
+		puts "That sure was a good #{cookie}"
+	end
 end
+
+santa_1 = Santa.new
+santa_1.speak
+santa_1.eat_milk_and_cookies("ladyfinger")
