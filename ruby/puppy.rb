255e4ea5d914e61c93b1277551ef72f4ff787ac0
Cory Mathis <coryrmathis@gmail.com>
2016-09-28 10:41:41 -0500

    Puppy class characteristics and behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+# **PUPPY CLASS**
+#
+# *CHARACTERISTICS*
+# 
+#  Bark Volume: medium/shrill
+#  Color: Brown and White
+#  Speed: 0-15 mph
+#  Tail Length: 7 in.
+#
+# *BEHAVIOR*
+#  
+#  Bark
+#  Run
+#  Catch
+#  Sniff
+#  Dig
+#
+#
+#
