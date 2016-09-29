1ebdf81a66e82e52fce36842c9f678272bd364f2
Cory Mathis <coryrmathis@gmail.com>
2016-09-28 20:38:39 -0500

    Consolidate printing work to santa initialization loopusing array storage and index accessibility

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,11 +1,14 @@
 class Santa
 
-	def initialize(gender, ethnicity)
+	attr_reader :ethnicity, :gender
+	attr_accessor :age
+
+	def initialize(gender, ethnicity, age)
 		puts "Initializing Santa instance..."
 		@gender = gender
 		@ethnicity = ethnicity
 		@reindeer_rating = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		@age = 0
+		@age = age
 	end
 
 	def speak
@@ -16,26 +19,20 @@ class Santa
 		puts "That sure was a good #{cookie}"
 	end
 
+# Advance age by one year of Santa instance
 	def celebrate_birthday
 		@age += 1
 	end
 
+# Moves given reindeer to end of rating list
 	def get_mad_at(reindeer)
 		reindeer_index = @reindeer_rating.index(reindeer)
 		@reindeer_rating.delete_at(reindeer_index)
 		@reindeer_rating[-1] = reindeer
 	end
-
-	def age
-		@age
-	end
-
-	def ethnicity
-		@ethnicity
-	end
 end
 
-# DRIVER CODE
+# SANTA CREATION PROGRAM
 
 # *Prepare empty santa container and example gender and ethnicity arrays.*
 
@@ -45,25 +42,34 @@ example_ethnicities = ["Hispanic", "White", "African-American", "Native American
 
 # *Random number generator to randomly assign gender and ethnicity to each
 # initialized santa. Random is a Ruby class. 0 <= random number < max integer.*
+
 prng = Random.new
-5.times do
-	santas << Santa.new(example_genders[prng.rand(example_genders.length)], example_ethnicities[prng.rand(example_ethnicities.length)])
+20.times do |i|
+	santas << Santa.new(example_genders[prng.rand(example_genders.length)], example_ethnicities[prng.rand(example_ethnicities.length)], prng.rand(140) )
+	puts "-" * 10
+	puts "This santa's age is #{santas[i].age}."
+	puts "They identify as #{santas[i].gender}."
+	puts "Their ethnicity is #{santas[i].ethnicity}."
+	puts "-" * 10
 end
+
+
+# DRIVER CODE
 
 # Test Instance Methods
 
 # Instance of Santa
-santa_1 = santas[0]
+# santa_1 = santas[0]
 
-p santa_1
-santa_1.speak
-santa_1.eat_milk_and_cookies("ladyfinger")
+# p santa_1
+# santa_1.speak
+# santa_1.eat_milk_and_cookies("ladyfinger")
 
-p santa_1
-santa_1.get_mad_at("Rudolph")
-santa_1.celebrate_birthday
+# p santa_1
+# santa_1.get_mad_at("Rudolph")
+# santa_1.celebrate_birthday
 
-p santa_1
-puts santa_1.ethnicity
-puts santa_1.age
+# p santa_1
+# puts santa_1.ethnicity
+# puts santa_1.age
 
