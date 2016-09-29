ddfea7772dc4960372d621190342951ac0011097
Cory Mathis <coryrmathis@gmail.com>
2016-09-28 19:50:27 -0500

    Update driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -37,24 +37,33 @@ end
 
 # DRIVER CODE
 
-# Testing instance methods
+# *Prepare empty santa container and example gender and ethnicity arrays.*
 
-# santa_1 = Santa.new
-# santa_1.speak
-# santa_1.eat_milk_and_cookies("ladyfinger")
 santas = []
 example_genders = ["male", "female", "shmale", "transgender", "agender"]
 example_ethnicities = ["Hispanic", "White", "African-American", "Native American", "Asian", "Asian-American"]
 
-# This random number generator takes a "max integer" parameter: 0 <= random number < max integer.
+# *Random number generator to randomly assign gender and ethnicity to each
+# initialized santa. Random is a Ruby class. 0 <= random number < max integer.*
 prng = Random.new
 5.times do
 	santas << Santa.new(example_genders[prng.rand(example_genders.length)], example_ethnicities[prng.rand(example_ethnicities.length)])
 end
 
+# Test Instance Methods
+
+# Instance of Santa
 santa_1 = santas[0]
+
+p santa_1
+santa_1.speak
+santa_1.eat_milk_and_cookies("ladyfinger")
+
 p santa_1
 santa_1.get_mad_at("Rudolph")
 santa_1.celebrate_birthday
+
 p santa_1
+puts santa_1.ethnicity
+puts santa_1.age
 
