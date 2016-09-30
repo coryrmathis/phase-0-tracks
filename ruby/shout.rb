cd9f77103a771a4d044e04b55081c355cfe80d82
Cory Mathis <coryrmathis@gmail.com>
2016-09-29 21:33:45 -0500

    Create mixin Shout module.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,41 @@
 module Shout
-
-	def self.yell_angrily(words)
-    	words + "!!!" + " :("
+	def censor_curse(words)
+		puts words.gsub(/[aeiou]/, "*")
 	end
-
-	def self.yelling_happily(words)
-		words + "!!!" + " :D"
-	end
-
 end
+
+class Auctioneer
+	include Shout
+end
+
+class Referee
+	include Shout
+end
+
 
 # DRIVER CODE
 
-puts Shout.yell_angrily("I'm angry")
-puts Shout.yelling_happily("I'm happy")
+auctioneer = Auctioneer.new
+auctioneer.censor_curse("Dammit")
+
+referee = Referee.new
+referee.censor_curse("Hellfire")
+
+
+
+# module Shout
+
+# 	def self.yell_angrily(words)
+#     	words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :D"
+# 	end
+
+# end
+
+# # DRIVER CODE
+
+# puts Shout.yell_angrily("I'm angry")
+# puts Shout.yelling_happily("I'm happy")
