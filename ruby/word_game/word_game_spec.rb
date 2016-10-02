4371459ec8ce360bdd29e50a52f0d00de8ed6d9f
Cory Mathis <coryrmathis@gmail.com>
2016-10-02 16:41:04 -0500

    1 of 9 tests passed. Initialize method stores word in instance variable and sets a guess count.  .word? returns the stored word. .guess_count returns current number guesses remaining.

--- ruby/word_game/word_game_spec.rb
+++ ruby/word_game/word_game_spec.rb
@@ -3,12 +3,16 @@ require_relative "word_game"
 describe WordGame do
 	let(:game) { WordGame.new("green")}
 
-	it "stores the word upon initialization" do
+	it "stores the word upon initialization and sets a guess count equal to the length of the word + 4" do
 		expect(game.word?).to eq "green"
+		expect(game.guess_count).to eq 9
 	end
 
-	it "sets a guess limit equal to length of word + 4" do
-		expect(game.guess_limit).to eq 9
+	it "updates the guess limit after a new guess" do
+		game.letter_guess("g")
+		expect(game.guess_count).to eq 8
+		game.letter_guess("g")
+		expect(game.guess_count).to eq 8
 	end
 
 	it "creates a string of underscores whose length equals length of word" do
@@ -18,6 +22,12 @@ describe WordGame do
 	it "accepts a letter guess and confirms or denies it's presence" do
 		expect(game.letter_guess("g")).to eq true
 		expect(game.letter_guess("a")).to eq false
+	end
+
+	it "checks if letter has been guessed already" do
+		game.letter_guess("g")
+		expect(game.check_letter("g")).to eq true
+		expect(game.check_letter("a")).to eq false
 	end
 
 	it "accepts a correct letter guess, updates @word_status, and @guessed_letters" do
@@ -31,13 +41,16 @@ describe WordGame do
 		game.letter_guess("a")
 		expect(game.incorrect_guess("a")).to eq "_____"
 		expect(game.word_status?).to eq "_____"
-		
+		expect(game.guessed_letters).to include("a")
+	end
 
+	it "prints congrats method if user wins" do
+		expect(game.win).to eq "Congrats, you won before you ran out of guesses"
+	end
 
-
-
-
-
+	it "prints consolation method if user loses and reveal word" do
+		expect(game.lose).to eq "Sorry, you're out of guesses.  The word was: green."
+	end
 
 
 
