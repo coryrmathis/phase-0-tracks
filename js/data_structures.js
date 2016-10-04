000bf7f45ad0c33afb21488b6daf8436e70edd37
Cory Mathis <coryrmathis@gmail.com>
2016-10-04 17:08:09 -0500

    Add horse_colors and horse_names arrays. Create functions for adding a color/horse to each

---
+++ js/data_structures.js
@@ -0,0 +1,10 @@
+var horse_colors = ["pink", "yellow", "green", "blue"];
+var horse_names = ["Freddie", "Calvin", "Ramona", "Michelle"];
+
+function addColor(color) {
+	horse_colors.push(color);
+}
+
+function addHorse(name) {
+	horse_names.push(name);
+}
